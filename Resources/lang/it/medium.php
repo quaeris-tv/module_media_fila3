d839942e78a32f8edec977ed026da89340a8362a
Davide Tricella <dadi.tricella@gmail.com>
2024-12-02 15:38:28 +0100

    .

---
+++ Resources/lang/it/medium.php
@@ -0,0 +1,43 @@
+<?php return array (
+  'actions' => 
+  array (
+    'create' => 
+    array (
+      'label' => 'create',
+    ),
+  ),
+  'fields' => 
+  array (
+    'collection_name' => 
+    array (
+      'label' => 'collection_name',
+    ),
+    'name' => 
+    array (
+      'label' => 'name',
+    ),
+    'mime_type' => 
+    array (
+      'label' => 'mime_type',
+    ),
+    'preview' => 
+    array (
+      'label' => 'preview',
+    ),
+    'human_readable_size' => 
+    array (
+      'label' => 'human_readable_size',
+    ),
+    'creator' => 
+    array (
+      'name' => 
+      array (
+        'label' => 'creator.name',
+      ),
+    ),
+    'created_at' => 
+    array (
+      'label' => 'created_at',
+    ),
+  ),
+);
