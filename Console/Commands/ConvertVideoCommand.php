6b898d5e1f5adb4333500d9c5f95c35cfcf6d54f
marco76tv <marco.sottana@gmail.com>
2024-12-19 23:40:07 +0100

    up

--- Console/Commands/ConvertVideoCommand.php
+++ Console/Commands/ConvertVideoCommand.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Modules\Media\Console\Commands;
 
+use FFMpeg\Format\Video\WebM;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
@@ -12,52 +13,40 @@ use Webmozart\Assert\Assert;
 
 class ConvertVideoCommand extends Command
 {
-    /** @var string */
     protected $signature = 'media:convert-video {disk} {file}';
-
-    /** @var string */
     protected $description = 'Convert Video';
 
-    /**
-     * Execute the console command.
-     *
-     * @return string
-     */
-    public function handle()
+    public function handle(): string
     {
         Assert::string($disk = $this->argument('disk'));
         Assert::string($file = $this->argument('file'));
         $this->info('disk: '.print_r($disk, true));
         $this->info('file: '.print_r($file, true));
-        // $this->error('');
-        // $this->line('Display this on the screen');
+
         if (! Storage::disk($disk)->exists($file)) {
             $this->error('['.$disk.'] file ['.$file.'] Not Exists');
 
             return '';
         }
-        $format = new \FFMpeg\Format\Video\WebM();
+
+        $format = new WebM();
         $extension = mb_strtolower(class_basename($format));
         $file_new = Str::of($file)
             ->replaceLast('.mp4', '.'.$extension)
             ->toString();
 
-        /*
-         * -preset ultrafast.
-         */
-        FFMpeg::fromDisk($disk)
-            ->open($file)
-            ->export()
-            // ->addFilter(function (VideoFilters $filters) {
-            //    $filters->resize(new \FFMpeg\Coordinate\Dimension(640, 480));
-            // })
-            // ->resize(640, 480)
-            ->onProgress(function (float $percentage, float $remaining, float $rate): void {
-                $this->info("{$percentage}% transcoded");
-                $this->info("{$remaining} seconds left at rate: {$rate}");
-            })
-            ->toDisk($disk)
+        $media = FFMpeg::fromDisk($disk)->open($file);
+        $export = $media->export();
+
+        $export->onProgress(function (float $percentage, float $remaining, float $rate): void {
+            $this->info("{$percentage}% transcoded");
+            $this->info("{$remaining} seconds left at rate: {$rate}");
+        });
+        // @phpstan-ignore method.nonObject, method.nonObject
+        $export->toDisk($disk)
+        // @phpstan-ignore method.nonObject
             ->inFormat($format)
+            // @phpstan-ignore method.nonObject
             ->save($file_new);
 
         return Storage::disk($disk)->url($file_new);
