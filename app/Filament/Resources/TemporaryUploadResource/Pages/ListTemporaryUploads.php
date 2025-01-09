9aba14d840365dc735d78023e827ff67466ed5ff
marco <marco.sottana@gmail.com>
2025-01-09 16:34:50 +0100

    up

--- app/Filament/Resources/TemporaryUploadResource/Pages/ListTemporaryUploads.php
+++ app/Filament/Resources/TemporaryUploadResource/Pages/ListTemporaryUploads.php
@@ -5,12 +5,30 @@ declare(strict_types=1);
 namespace Modules\Media\Filament\Resources\TemporaryUploadResource\Pages;
 
 use Filament\Actions\CreateAction;
+use Filament\Tables\Columns\TextColumn;
 use Modules\Media\Filament\Resources\TemporaryUploadResource;
 use Modules\Xot\Filament\Resources\Pages\XotBaseListRecords;
 
 class ListTemporaryUploads extends XotBaseListRecords
 {
     protected static string $resource = TemporaryUploadResource::class;
+
+    public function getListTableColumns(): array
+    {
+        return [
+            'folder' => TextColumn::make('folder')
+                ->searchable()
+                ->sortable()
+                ->wrap(),
+            'filename' => TextColumn::make('filename')
+                ->searchable()
+                ->sortable()
+                ->wrap(),
+            'created_at' => TextColumn::make('created_at')
+                ->dateTime()
+                ->sortable(),
+        ];
+    }
 
     /**
      * @return CreateAction[]
