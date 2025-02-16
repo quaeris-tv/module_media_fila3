67c5288c845b699c2db7b163791ffd5ca37a754d
Malebestia <vair81@gmail.com>
2025-02-16 10:09:48 +0100

    fix(EditMedia*): remove XotBaseEditRecord inheritance and use EditRecord directly for EditMedia, EditMediaConvert, and EditTemporaryUpload pages

--- App/Filament/Resources/MediaResource/Pages/EditMedia.php
+++ App/Filament/Resources/MediaResource/Pages/EditMedia.php
@@ -8,11 +8,7 @@ use Filament\Actions\DeleteAction;
 use Filament\Resources\Pages\EditRecord;
 use Modules\Media\Filament\Resources\MediaResource;
 
-<<<<<<< HEAD
 class EditMedia extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
-=======
-class EditMedia extends EditRecord
->>>>>>> 5b301225981f0c2116c7e0b5bea444099a08bfd7
 {
     protected static string $resource = MediaResource::class;
 
