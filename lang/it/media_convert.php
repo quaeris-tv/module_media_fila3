b19a378c41f8815347910a9bc1a47642fe0569e4
marco76tv <marco.sottana@gmail.com>
2025-02-17 19:24:34 +0100

    up

---
+++ lang/it/media_convert.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'navigation' => 
+  array (
+    'label' => 'media convert.navigation',
+    'group' => 'media convert.navigation',
+    'icon' => 'media convert.navigation',
+    'sort' => 100,
+  ),
+);
---
+++ lang/it/temporary_upload.php
@@ -0,0 +1,9 @@
+<?php return array (
+  'navigation' => 
+  array (
+    'label' => 'temporary upload.navigation',
+    'group' => 'temporary upload.navigation',
+    'icon' => 'temporary upload.navigation',
+    'sort' => 72,
+  ),
+);
