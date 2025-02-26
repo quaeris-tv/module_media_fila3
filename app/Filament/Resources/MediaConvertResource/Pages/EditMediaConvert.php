b9a4162b0795d58967d2ca41929d316f45a70d55
marco76tv <marco.sottana@gmail.com>
2025-02-26 15:45:16 +0100

    Auto update submodule

--- app/Filament/Resources/MediaConvertResource/Pages/EditMediaConvert.php
+++ app/Filament/Resources/MediaConvertResource/Pages/EditMediaConvert.php
@@ -4,12 +4,9 @@ declare(strict_types=1);
 
 namespace Modules\Media\Filament\Resources\MediaConvertResource\Pages;
 
-use Filament\Actions;
 use Modules\Media\Filament\Resources\MediaConvertResource;
 
 class EditMediaConvert extends \Modules\Xot\Filament\Resources\Pages\XotBaseEditRecord
 {
     protected static string $resource = MediaConvertResource::class;
-
-   
 }
